7c2e75abfa7f67f43ad107a72a774e545dde307c
Alejandro Armadillo <alex.cel.lemon@gmail.com>
2022-10-26 18:08:34 -0500

    Agregando los test al proyecto para mas control

--- booking/serializers.py
+++ booking/serializers.py
@@ -2,22 +2,22 @@ from dataclasses import field
 from booking.models import User, Room, Event, Booking
 from rest_framework import serializers
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'role',]
 
-class RoomSerializer(serializers.HyperlinkedModelSerializer):
+class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ['url', 'id', 'capacity',]
     
-class EventSerializer(serializers.HyperlinkedModelSerializer):
+class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = ['url', 'title', 'room', 'date', 'type',]
 
-class BookingSerializer(serializers.HyperlinkedModelSerializer):
+class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
         fields = ['url', 'event', 'user']
--- booking/tests.py
+++ booking/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
 
-# Create your tests here.
+class BookingTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_room(self):
+        data = {"capacity": 10}
+        response = self.client.post('/rooms/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_event(self):
+        data = {
+            "title": "Titulo del evento",
+            "room": 1,
+            "date": "2022-10-10"
+        }
+        response = self.client.post('/events/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED, response.data)
+
